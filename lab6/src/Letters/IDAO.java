832f489e86fa1727425547cc9668c35f7cbe9f15
ksesha <72476438+crinanis@users.noreply.github.com>
2022-05-09 17:41:03 +0300

    Add files via upload

---
+++ lab6/src/Letters/IDAO.java
@@ -0,0 +1,10 @@
+package Letters;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface IDAO {
+    public Connection getConnection() throws SQLException;
+    public IGenDAO <Letter, Integer> getLetterDAO(Connection connection);
+    public  IGenDAO<Person, Integer> getPersonDAO(Connection connection);
+}
---
+++ lab6/src/Letters/Person.java
@@ -0,0 +1,22 @@
+package Letters;
+
+public class Person {
+    public String snp;
+    public String dateBirth;
+    public String sends;
+    public String receives;
+
+    public Person(){
+        snp = "no info";
+        dateBirth = "no info";
+        sends = "no info";
+        receives = "no info";
+    }
+
+    @Override
+    public String toString() {
+        return "\nAuthor{ " +
+                "Surname name patronymic='" + snp + '\'' + ", dateBirth='" + dateBirth + '\'' + ", sends='" + sends + '\'' + ", receives='" + receives + " " +
+                '}';
+    }
+}
